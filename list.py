a736f866736edb0e2da93896674c1b77463b7f56
iamithakur <amithakur62500@gmail.com>
2026-01-29 19:55:08 +0530

    +1

---
+++ list.py
@@ -0,0 +1,16 @@
+# List = Collection of items that is 
+# Ordered,
+# Mutable(changable), 
+# Allows duplicate values,
+# Written using square brackets i.e.,[]
+
+marks = [85, 92, 78, 96, 88]
+print(marks) # Output: [85, 92, 78, 96, 88]
+
+print(marks[0]) # Display the particular element at the mentioned index ,Output: 85
+print(marks[3]) # Output: 96
+
+marks[3]=100 # Update the element at index 3
+print(marks) # Display the updated list
+
+## In Python, The List can store elements of different datatypes(integers, floats, strings, etc...)
